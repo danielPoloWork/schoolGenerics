2f362176f7a307f48816bd17b210a9156c4e0504
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-07-05 10:06:44 +0200

    Update repository RPRepository.java - Implemented  Interface Repository<of Interface Entity>
    public class RPRepository implements INRepository<INEntity>
    
    Imported all methods @Override
    Created instance Map<String, INEntity>
    Created constructor that returns an HashMap

--- src/main/java/repositories/RPRepository.java
+++ src/main/java/repositories/RPRepository.java
@@ -1,5 +1,37 @@
 package repositories;
 
-public class RPRepository {
+import interfaces.INEntity;
+import interfaces.INRepository;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
+public class RPRepository implements INRepository<INEntity> {
+
+  private final Map<String, INEntity> databaseTable;
+
+  public RPRepository() {
+    this.databaseTable = new HashMap<>();
+  }
+
+  @Override
+  public INEntity insertObject(INEntity objectArg) {
+    return null;
+  }
+
+  @Override
+  public Optional<INEntity> findById(String objectId) {
+    return Optional.empty();
+  }
+
+  @Override
+  public String generateRandomId() {
+    return null;
+  }
+
+  @Override
+  public Collection<INEntity> findAllRecords() {
+    return null;
+  }
 }
