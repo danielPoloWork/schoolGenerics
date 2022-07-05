89f86c20f5350f171f468e710dc880a338f43685
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-07-05 09:59:22 +0200

    Update entity Lesson.Java by implementing lombok @Getter @AllArgsConstructor

--- src/main/java/entities/Lesson.java
+++ src/main/java/entities/Lesson.java
@@ -1,54 +1,21 @@
 package entities;
 
+import enums.ENSubjectType;
 import interfaces.INEntity;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
 
+@AllArgsConstructor
+@Getter
 public class Lesson implements INEntity {
 
   // DECLARATION
   private final String id;
   private final String dateTimeEnd;
   private final String dateTimeStart;
-  private final ENSubjectType ENSubjectType;
+  private final ENSubjectType subject;
   private final List<String> students;
   private final String teacherId;
-
-  // CONSTRUCTOR
-  public Lesson(String idArg, String dateTimeEndArg, String dateTimeStartArg,
-      ENSubjectType classIdArg, List<String> studentIdArg, String teacherIdArg) {
-    this.id = idArg;
-    this.dateTimeEnd = dateTimeEndArg;
-    this.dateTimeStart = dateTimeStartArg;
-    this.ENSubjectType = classIdArg;
-    this.students = studentIdArg;
-    this.teacherId = teacherIdArg;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public String getDateTimeEnd() {
-    return dateTimeEnd;
-  }
-
-  public String getDateTimeStart() {
-    return dateTimeStart;
-  }
-
-  public List<String> getStudents() {
-    return students;
-  }
-
-  public String getTeacherId() {
-    return teacherId;
-  }
-
-  public ENSubjectType getSubjectTypeEnum() {
-    return ENSubjectType;
-  }
-
-  public boolean isStudentAttending(String studentId) {
-    return students.contains(studentId);
-  }
 }
